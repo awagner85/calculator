aa7b5acf4a0279e3b63e328e7297995daa8fb000
Amanda Wagner <aawagner85@gmail.com>
2014-08-28 10:10:48 -0700

    Modified calculator app

--- calculator1.rb
+++ calculator1.rb
@@ -1,22 +1,24 @@
-puts "What is the first number?"
-num1 =  gets.chomp
+loop do 	
+	puts "What is the first number?"
+	num1 =  gets.chomp
 
-puts "What is the second number?"
-num2 = gets.chomp
+	puts "What is the second number?"
+	num2 = gets.chomp
+
+		puts "Would you like to add, subtract, multiply, or divide your two numbers?"
+		op = gets.chomp
   
-puts "Would you like to add, subtract, multiply, or divide your two numbers?"
-op = gets.chomp
-  
-if op == "add"
-  answer = num1.to_i + num2.to_i
-elsif op == "subtract"
-  answer = num1.to_i - num2.to_i
-elsif op == "multiply"
-  answer = num1.to_i * num2.to_i
-elsif op == "divide"
-  answer = num1.to_f / num2.to_f
-else
-  puts "Please choose one of the operations."
+		if op == "add"
+  		answer = num1.to_i + num2.to_i
+		elsif op == "subtract"
+  		answer = num1.to_i - num2.to_i
+		elsif op == "multiply"
+  		answer = num1.to_i * num2.to_i
+		elsif op == "divide"
+  		answer = num1.to_f / num2.to_f
+		else
+  		puts "=> Let's try that again."
+  		redo
+		end
+			puts "The answer is #{answer}."
 end
-
-puts "The answer is #{answer}."
