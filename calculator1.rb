f6628213bf0c067557ecb23ebc213ea2653224d8
Amanda Wagner <aawagner85@gmail.com>
2014-08-28 09:30:05 -0700

    Added calculator app

---
+++ calculator1.rb
@@ -0,0 +1,22 @@
+puts "What is the first number?"
+num1 =  gets.chomp
+
+puts "What is the second number?"
+num2 = gets.chomp
+  
+puts "Would you like to add, subtract, multiply, or divide your two numbers?"
+op = gets.chomp
+  
+if op == "add"
+  answer = num1.to_i + num2.to_i
+elsif op == "subtract"
+  answer = num1.to_i - num2.to_i
+elsif op == "multiply"
+  answer = num1.to_i * num2.to_i
+elsif op == "divide"
+  answer = num1.to_f / num2.to_f
+else
+  puts "Please choose one of the operations."
+end
+
+puts "The answer is #{answer}."
